b1a5f002dc7d6869971544e67b724c3fea2e86cc
unknown <mohan.moturi@logikal.ai>
2026-05-12 18:40:52 +0530

    update JWT

--- backend/app.js
+++ backend/app.js
@@ -116,7 +116,7 @@ const verifyToken = (req, res, next) => {
     req.user = decoded;
     next();
   } catch (error) {
-    return res.status(403).json({ message: "Invalid or expired token" });
+    return res.status(403).json({ message: "Invalid or expired JWTtoken" });
   }
 };
 
